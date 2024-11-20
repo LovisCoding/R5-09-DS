3ff360a2b37e1b4944318112f12103bf80d77329
Arthur <120005604+LovisCoding@users.noreply.github.com>
2024-11-20 14:01:21 +0100

    Creation Q3.java

---
+++ Q3.java
@@ -0,0 +1,17 @@
+public class Q3 {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            try {
+                int numberOfStars = Integer.parseInt(args[0]);
+                for (int i = 0; i < numberOfStars; i++) {
+                    System.out.print("*");
+                }
+                System.out.println(); // Saut de ligne après les étoiles
+            } catch (NumberFormatException e) {
+                System.out.println("Fournissez un entier valide en paramètre.");
+            }
+        } else {
+            System.out.println("Veuillez fournir un paramètre pour le nombre d'étoiles.");
+        }
+    }
+}
